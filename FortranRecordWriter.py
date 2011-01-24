2acdbf93e9aa7dbf5b381ef43ab72711fc09a9e7
brendan@brendan-arnolds-macbook.local <brendan@brendan-arnolds-macbook.local>
2011-01-24 00:54:57 +0000

    FortranRecordWriter  now functions with the new code

--- FortranRecordWriter.py
+++ FortranRecordWriter.py
@@ -1,21 +1,36 @@
 from _output import output
+from _lexer import lexer
 from _parser import parser
 
 class FortranRecordWriter(object):
     '''
     Generate a writer object for FORTRAN format strings
+
+    Typical use case ...
+
+    >>> header_line = FortranRecordWriter("(A15, A15, A15)")
+    >>> header_line.write(['x', 'y', 'z'])
+    '              x              y              z'
+    >>> line = FortranRecordWriter('(3F15.3)')
+    >>> line.write([1.0, 0.0, 0.5])
+    '          1.000          0.000          0.500'
+    >>> line.write([1.1, 0.1, 0.6])
+    '          1.100          0.100          0.600'
+
+    Note: it is best to create a new object for each format, changing the format
+    causes the parser to reevalute the format string which is costly in terms of
+    performance
     '''
-    def __init__(self, format, version=None):
-        self.version = version
-        self.format = format
+    def __init__(self, format):
+        self._lexer = lexer
         self._parser = parser
         self._edit_descriptors = []
         self._overflow_edit_descriptors = []
-        self._parse_format()
+        self.format = format
 
     def __eq__(self, other):
-        if type(other) == str:
-            return self.match(other) 
+        if isinstance(other, FortranRecordWriter):
+            return self.format == other.format
         else:
             return object.__eq__(self, other)
 
@@ -24,7 +39,7 @@ class FortranRecordWriter(object):
         Pass a list of values correspoding to the FORTRAN format specified
         to generate a string
         '''
-        return _output(self.edit_descriptors, self._overflow_edit_descriptors, values)
+        return output(self._edit_descriptors, self._overflow_edit_descriptors, values)
 
     def get_format(self):
         return self._format
@@ -32,22 +47,15 @@ class FortranRecordWriter(object):
     def set_format(self, format):
         self._format = format
         self._parse_format()
+
     format = property(get_format, set_format)
 
-    def get_version(self):
-        return self._version
-    def set_version(self, version):
-        self._version = version
-        self._parse_format()
-    version = property(get_version, set_version)
-
     def _parse_format(self):
-        self._edit_descriptors, self._overflow_edit_descriptors = self._parser(self.format, self.version)
+        tokens = self._lexer(self.format)
+        self._edit_descriptors, self._overflow_edit_descriptors = self._parser(tokens)
 
 
 if __name__ == '__main__':
     import doctest
-    import os
     doctest.testmod()
-##     doctest.testfile(os.path.join('tests', 'FortranRecordWriter_test.txt'))
 
