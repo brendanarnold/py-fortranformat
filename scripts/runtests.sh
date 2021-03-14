64c555a9b23dabfaf8a7a6492eb8720d21df210b
Brendan Arnold <brendanarnold@gmail.com>
2021-03-14 17:35:06 +0000

    Tidy up test location

--- scripts/runtests.sh
+++ scripts/runtests.sh
@@ -1,1 +1,2 @@
-nosetests tests/autogen/output/ifort/9-1_linux_intel tests/autogen/input/ifort/9-1_linux_intel tests/misc
+# Note that an empty newline at the bottom of the file will break the nosetest execution for some reason
+nosetests tests/handwritten tests/autogen/input/ifort/9-1_linux_intel tests/autogen/output/ifort/9-1_linux_intel
