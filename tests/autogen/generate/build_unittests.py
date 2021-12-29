ae1f4ef43dd70d049699b3290852f1022fea2b56
Brendan Arnold <brendanarnold@gmail.com>
2021-12-29 03:36:23 +0000

    New test naming

--- tests/autogen/generate/build_unittests.py
+++ tests/autogen/generate/build_unittests.py
@@ -16,9 +16,8 @@ INPUT_TEST_FILESTEM = r'(\w+)-ed-input-(\d+)\.test'
 # Output tests may have one or two edit descriptors hence the weird
 # prefix
 OUTPUT_TEST_FILESTEM = r'((\w+-)?\w+)-ed-output\.test'
-INPUT_UNITTEST_FILESTEM = '%s-input-test-%d.py'
-OUTPUT_UNITTEST_FILESTEM = '%s-output-test.py'
-
+INPUT_UNITTEST_FILESTEM = 'test_%s_input_%d.py'
+OUTPUT_UNITTEST_FILESTEM = 'test_%s_output.py'
 
 # Start with the input tests
 for filepath, subdirs, dummy in os.walk(INPUT_DIR):
@@ -35,9 +34,10 @@ for filepath, subdirs, dummy in os.walk(INPUT_DIR):
         name = res.group(1)
         batch = int(res.group(2))
         infile = os.path.join(raw_filepath, fn)
-        outfile = os.path.join(filepath, INPUT_UNITTEST_FILESTEM % (name, batch))
+        outfile = os.path.join(
+            filepath, INPUT_UNITTEST_FILESTEM % (name, batch))
         write_input_unittest(infile, outfile, batch, name, platform)
-        
+
 # Now output tests
 for filepath, subdirs, dummy in os.walk(OUTPUT_DIR):
     # Identify by the raw subdirectory
@@ -54,4 +54,3 @@ for filepath, subdirs, dummy in os.walk(OUTPUT_DIR):
         infile = os.path.join(raw_filepath, fn)
         outfile = os.path.join(filepath, OUTPUT_UNITTEST_FILESTEM % name)
         write_output_unittest(infile, outfile, name, platform)
-    
