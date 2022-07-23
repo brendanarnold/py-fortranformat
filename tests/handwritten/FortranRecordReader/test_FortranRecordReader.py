78a35780770f2985f6b57ff0f0ce0031e2a9f7c7
Brendan Arnold <brendanarnold@gmail.com>
2022-07-23 10:15:10 +0100

    Test change

--- tests/handwritten/FortranRecordReader/test_FortranRecordReader.py
+++ tests/handwritten/FortranRecordReader/test_FortranRecordReader.py
@@ -7,6 +7,6 @@ import fortranformat as ff
 class FortranRecordWriterTests(unittest.TestCase):
 
     def test_1(self):
-        '''General test of FortranRecordReader'''
+        '''Smoke test of FortranRecordReader'''
         record_line = ff.FortranRecordReader('(I4, I4, I4)')
         self.assertEqual(record_line.read('  12   34567'), [12, 3, 4567])
