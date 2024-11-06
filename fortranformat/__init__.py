a16959e99b28464b0bb0c9492b6f93162001bcef
Tom Body <tbody@cfs.energy>
2024-11-06 16:57:20 -0500

    Backward compatibility: add __version__ to __init__.py

--- fortranformat/__init__.py
+++ fortranformat/__init__.py
@@ -2,3 +2,15 @@ from .FortranRecordReader import FortranRecordReader
 from .FortranRecordWriter import FortranRecordWriter
 from ._exceptions import InvalidFormat
 from . import config
+
+# Read in the 'version' tag of the installed package
+import importlib.metadata
+__version__ = importlib.metadata.version("fortranformat")
+
+__all__ = [
+    "FortranRecordReader",
+    "FortranRecordWriter",
+    "InvalidFormat",
+    "config",
+    "__version__",
+]
